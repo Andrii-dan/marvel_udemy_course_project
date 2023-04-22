702b551a24c4918b750eff8e7fbf81903ddca5ac
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2023-04-22 23:34:16 +0200

    single comics page added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,10 @@
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import AppHeader from '../appHeader/AppHeader';
 import MainPage from '../pages/MainPage';
 import ComicsPage from '../pages/ComicsPage';
+import SingleComicsPage from '../pages/SingleComicsPage';
+import Page404 from '../pages/404';
 
 const App = () => {
 	return (
@@ -12,6 +15,8 @@ const App = () => {
 					<Routes>
 						<Route path='/' element={<MainPage />} />
 						<Route path='/comics' element={<ComicsPage />} />
+						<Route path='/comics/:comicsId' element={<SingleComicsPage />} />
+						<Route path='*' element={<Page404 />} />
 					</Routes>
 				</main>
 			</div>
